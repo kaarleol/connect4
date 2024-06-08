5d5cc647e9ef842e81e50dc1a6346907103ee5b8
kaarleol <kaarlenloota@gmail.com>
2024-06-08 20:36:41 +0300

    syncing tests between devices

--- src/tests/board_test.py
+++ src/tests/board_test.py
@@ -5,5 +5,12 @@ class TestBoard(unittest.TestCase):
     def setUp(self):
         self.test_board = Board()
 
-    def test_board_sets_up_correctly(self):
+    def test_board_sets_up_empty_board(self):
+        self.assertEqual(len(self.test_board.get_board()), 6)
         self.assertEqual(len(self.test_board.get_board()[0]), 7)
+        
+        board = self.test_board.get_board()
+
+        for i in range(6):
+            for j in range(7):
+                self.assertEqual(board[i][j], ' ')
